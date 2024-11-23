4b9f6d620c4fc3fab9f9a7f930410ac9e3f9f863
Fitsum <122894183+thecryptobeserker@users.noreply.github.com>
2024-11-22 20:46:16 -0500

    Updated BSTNode file

--- Assignment 3/BSTNode.h
+++ Assignment 3/BSTNode.h
@@ -1,8 +1,11 @@
 #include <iostream>
-#include <string>
-#include <string>
+#include <fstream>
 #include <vector>
-
+#include <list>
+#include <queue>
+#include <sstream>
+#ifndef BSTNODE_H
+#define BSTNODE_H
 
 class BSTNode {
 public:
@@ -12,3 +15,4 @@ public:
 
     BSTNode(const std::string& value) : data(value), left(nullptr), right(nullptr) {}
 };
+#endif
