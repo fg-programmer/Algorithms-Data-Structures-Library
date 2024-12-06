ac0846733e54b21b18863d8a78ca295c352d3c15
Fitsum <122894183+thecryptobeserker@users.noreply.github.com>
2024-12-06 11:21:17 -0500

    Commit #2
    Almost done with Bellman-ford algorithm, moved on to writing a function which will parse the graph2.txt file

--- Assignment 4/Graph.h
+++ Assignment 4/Graph.h
@@ -36,3 +36,42 @@ void bellmanFord(Graph& graph, int source) {
 
     distance[source] = 0;
 
+    // Relax edges up to V-1 times
+    for (int i = 1; i <= V - 1; ++i) {
+        for (const Edge& edge : edges) {
+            if (distance[edge.start] != numeric_limits<int>::max() &&
+                distance[edge.start] + edge.weight < distance[edge.end]) {
+                distance[edge.end] = distance[edge.start] + edge.weight;
+                predecessor[edge.end] = edge.start;
+            }
+        }
+    }
+
+    // Check for negative weight cycles
+    for (const Edge& edge : edges) {
+        if (distance[edge.start] != numeric_limits<int>::max() &&
+            distance[edge.start] + edge.weight < distance[edge.end]) {
+            cout << "Graph contains a negative weight cycle." << endl;
+            return;
+        }
+    }
+
+
+
+
+Graph parseGraph(const string& inputFile) {
+    ifstream file(inputFile);
+    string line;
+    Graph* graph = nullptr;
+    while (getline(file, line)) {
+        istringstream iss(line);
+        string command;
+        iss >> command;
+
+        if (command == "new") {
+            int numVertices;
+            graph = new Graph(numVertices);
+        } else if (command == "add") {
+            string type;
+            iss >> type;
+
