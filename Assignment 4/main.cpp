78d722b73d6fc3fd6fa95447606dc9da922919fd
Fitsum <122894183+thecryptobeserker@users.noreply.github.com>
2024-12-06 23:59:56 -0500

    Final code commit
    Integrated both files

--- Assignment 4/main.cpp
+++ Assignment 4/main.cpp
@@ -1,9 +1,20 @@
+#include "Graph.h"
 #include "Spice.h"
 
 int main() {
+    vector<string> files = {"graphs2.txt"};
+    for (const auto& file : files) {
+        Graph graph = parseGraph(file);
+        cout << "Running Bellman-Ford on graph from " << file << endl;
+        bellmanFord(graph, 1); // Run SSSP from vertex #1
+    }
+
     vector<Spice> spices;
-    vector<int> knapsack_cpcity;
-    parseInput("spice.txt", spices, knapsack_cpcity);
-    fractionalKnapsack(spices, knapsack_cpcity);
+    vector<int> knapsack_capacities;
+
+    parseInput("spice.txt", spices, knapsack_capacities);
+
+    fractionalKnapsack(spices, knapsack_capacities);
+
     return 0;
 }
